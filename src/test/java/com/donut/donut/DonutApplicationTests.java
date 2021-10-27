3bca16284cdde2051b03c035c754bc767c14b82b
서건희 <seogunhee4@gmail.com>
2021-10-28 08:42:38 +0900

    [FIX]

--- src/test/java/com/donut/donut/DonutApplicationTests.java
+++ src/test/java/com/donut/donut/DonutApplicationTests.java
@@ -3,7 +3,7 @@ package com.donut.donut;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class DonutApplicationTests {
 
     @Test
