9b7f3e40a71ab4d1178b77d88504521ae5863439
서건희 <seogunhee4@gmail.com>
2021-11-01 22:14:08 +0900

    [FIX]

--- src/main/java/com/donut/donut/payload/response/DoneResponse.java
+++ src/main/java/com/donut/donut/payload/response/DoneResponse.java
@@ -3,6 +3,7 @@ package com.donut.donut.payload.response;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
@@ -11,7 +12,7 @@ public class DoneResponse {
     private Long doneId;
     private String userName;
     private Long kakaoId;
-    private LocalDateTime writeAt;
+    private LocalDate writeAt;
     private String title;
     private String content;
     private Boolean isPublic;
